1c24bc4a3987a84425cade12ab52a77ef4dc4c6e
Omar Elsayed <eng.omar.elsayed@hotmail.com>
2024-07-31 00:14:31 +0300

    Updated the package.swift file

--- Sources/SwiftToastAlert/SwiftToastAlert.swift
+++ Sources/SwiftToastAlert/SwiftToastAlert.swift
@@ -1,2 +1,18 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  SwiftToastAlert.swift
+//  
+//
+//  Created by Eng.Omar Elsayed on 31/07/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
