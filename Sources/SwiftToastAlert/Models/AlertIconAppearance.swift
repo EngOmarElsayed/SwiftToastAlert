2428d07dfe4f0b8b971e4c5fc9af9088a2e74a0a
Omar Elsayed <eng.omar.elsayed@hotmail.com>
2024-07-31 22:10:11 +0300

    Finished the documentation of the `AlertIconAppearance`

--- Sources/SwiftToastAlert/Models/AlertIconAppearance.swift
+++ Sources/SwiftToastAlert/Models/AlertIconAppearance.swift
@@ -7,12 +7,17 @@
 
 import SwiftUI
 
+/// This type represent the icon the will be displayed in the alert and it's properties.
 public struct AlertIconAppearance {
+  /// This represent the icon that will be displayed in the alert, it can be SF symbol or image from the asset catalog.
   public let image: Image
+  /// This is the color of the icon in the alert, it is used for SF symbol only.
   public let color: Color?
+  /// The font properties is used to customize the size and weight of the icon if it was SF symbol
   public let font: Font?
   
-  public init(image: Image, color: Color?, font: Font? = nil) {
+  /// if you are not using SF symbol, don't init the color or the font property.
+  public init(image: Image, color: Color? = nil, font: Font? = nil) {
     self.image = image
     self.color = color
     self.font = font
