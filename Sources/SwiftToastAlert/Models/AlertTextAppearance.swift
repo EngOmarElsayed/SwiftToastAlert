813b7a1c0562282349cf9f40ad1e7a5a55700db0
Omar Elsayed <eng.omar.elsayed@hotmail.com>
2024-07-31 22:18:21 +0300

    Finished the documentation of the AlertTextAppearance

--- Sources/SwiftToastAlert/Models/AlertTextAppearance.swift
+++ Sources/SwiftToastAlert/Models/AlertTextAppearance.swift
@@ -7,9 +7,13 @@
 
 import SwiftUI
 
+/// This represent the text that will be displayed in the alert toast with it's properties.
 public struct AlertTextAppearance {
+  /// The displayed text in the toast alert.
   public let text: String
+  /// The color of the displayed ``text``.
   public let color: Color
+  /// The font properties of the displayed text, that contains size, weight and font of the ``text``.
   public let font: Font
   
   public init(text: String, color: Color, font: Font = .system(size: 16, weight: .bold)) {
